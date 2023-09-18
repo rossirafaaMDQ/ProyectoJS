ef4c94137b7c3cd9a766d552da6d3961bd8b9889
FitaCoder <rossirafaaprogram@gmail.com>
2023-09-18 01:10:25 -0300

    agregue switch

--- main.js
+++ main.js
@@ -2,22 +2,28 @@ let nombre = prompt("Ingrese su nombre");
 
 let apellido = prompt("Apellido?");
 
-if (nombre && nombre) {
+if (nombre.trim() === "") {
+  alert("nombrenoválido");
+}
+
+if (nombre != "") {
   console.log("Hola, " + nombre);
-} else if (nombre != "") {
-  console.log("Por favor, Ingrese su nombre");
 } else {
   console.log("Por favor, Ingrese su nombre");
 }
 
-if (apellido && apellido) {
-} else if (apellido != "") {
-  console.log("Vuelva a ingresar su apellido");
+if (apellido != "") {
 } else {
   console.log("Vuelva a ingresar su apellido");
 }
 
-alert("Bienvenido, " + nombre + " " + apellido);
+alert(
+  "Bienvenido, " +
+    nombre +
+    " " +
+    apellido +
+    " , esta ingresando a el banco Money. "
+);
 
 let documento = parseInt(
   prompt("Ahora su numero de documento para poder ingresar")
@@ -33,26 +39,42 @@ if (documento > 11111111) {
 
 console.log(documento);
 
-// parte de calculo
-//desea hacer la operacion en dolares o euros
-
 let pesos = parseInt(prompt("Escriba el total en pesos que desea invertir"));
-
-// pesos = 300.000
 
 let dolarHoy = parseInt(prompt("Ingrese cuanto esta el dolar"));
 
-// dolarHoy = 730
-
-//let euroHoy = parseInt(prompt("Escriba a cuanto esta el euro hoy"));
-
-//euroHoy = 800
-
 let total = pesos * dolarHoy;
 
-alert(pesos + " x " + dolarHoy + " = " + total);
+let inversion = confirm(" El total es, " + total + " queres comprar? ");
 
-//este es el total, desea comprar ?
-//si / no
-// si = este es el total
-//no = Muchas gracias
+if (inversion == true) {
+  let transaccion = prompt(
+    " donde desea que el dinero sea depositado?, Cuenta bancaria, efectivo o plazo fijo"
+  );
+  /*
+  let deposito = prompt(" su dinero sera transferido a: " + transaccion);
+*/
+  switch (transaccion) {
+    case "cuenta bancaria":
+      alert(
+        "lo esperamos entre el dia 10 y 15 a partir de las 10 hs hasta las 20hs para confirmar esta transaccion, gracias por confiar en banco Money"
+      );
+      break;
+
+    case "efectivo":
+      alert(
+        "su dinero puede ser transferido dentro de 48hs, gracias por confiar en banco Money"
+      );
+      break;
+
+    case "plazo fijo":
+      alert(
+        "el dinero sera ingresado a un nuevo plazo fijo, para confirmar esta accion y programar la cantidad de dias ingrese en el link que enviamos a su Gmail, gracias por confiar en banco Money"
+      );
+
+    default:
+      alert(
+        "No tenemos disponible esa opcion, para cualquier consulta llame al 0800-111-5050, gracias"
+      );
+  }
+}
