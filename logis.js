6bf84c982718139772e4b5302b0d587f043bc004
FitaCoder <rossirafaaprogram@gmail.com>
2023-11-19 11:42:50 -0300

    Cambio de proyecto (Login)

---
+++ logis.js
@@ -0,0 +1,40 @@
+function verificarCredenciales() {
+  const usuario = document.getElementById("usuario").value.toLowerCase();
+  const contraseña = document.getElementById("contraseña").value;
+  console.log(usuario);
+  if (usuario === "Rafamdp" && contraseña === "coder123") {
+    Swal.mixin({
+      toast: true,
+      position: "top",
+      showConfirmButton: false,
+      timer: 2000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener("mouseenter", Swal.stopTimer);
+        toast.addEventListener("mouseleave", Swal.resumeTimer);
+      },
+    }).fire({
+      icon: "success",
+      title: "Inicio de sesion exitosa!",
+    });
+    setTimeout(() => {
+      window.location.href = "inventario.html";
+      sessionStorage.setItem("nombreUsuario", usuario);
+    }, 2100);
+  } else {
+    Swal.mixin({
+      toast: true,
+      position: "top",
+      showConfirmButton: false,
+      timer: 2000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener("mouseenter", Swal.stopTimer);
+        toast.addEventListener("mouseleave", Swal.resumeTimer);
+      },
+    }).fire({
+      icon: "error",
+      title: "Credenciales incorrectas",
+    });
+  }
+}
